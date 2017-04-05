e825d65e4322d2be7ad7de9d6482a00ea882c0f0
lithiumyoung <LIYANG1937771949>
2017-04-05 10:25:08 +0800

    第一次提交的版本

---
+++ app/src/main/java/com/young/ganknowledge/ui/BaseActivity.java
@@ -0,0 +1,24 @@
+package com.young.ganknowledge.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lithiumyoung on 2017/3/23.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+
+
+    }
+}
